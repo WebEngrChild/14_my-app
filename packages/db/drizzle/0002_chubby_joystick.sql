d9f17729f87abc7b2513cb5675a3545ab9f7691c
WebEngChild <WebEngChild@example.com>
2026-09-20 09:00:48 +0900

    feat(db): add todo creation timestamp

---
+++ packages/db/drizzle/0002_chubby_joystick.sql
@@ -0,0 +1,1 @@
+ALTER TABLE "todos" ADD COLUMN "created_at" timestamp with time zone DEFAULT now() NOT NULL;
--- packages/db/schema.ts
+++ packages/db/schema.ts
@@ -1,9 +1,12 @@
-import { integer, pgTable, text } from "drizzle-orm/pg-core";
+import { integer, pgTable, text, timestamp } from "drizzle-orm/pg-core";
 
 export const todos = pgTable("todos", {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
   title: text().notNull(),
   body: text().notNull(),
+  createdAt: timestamp("created_at", { withTimezone: true })
+    .defaultNow()
+    .notNull(),
 });
 
 export * from "./auth-schema";
